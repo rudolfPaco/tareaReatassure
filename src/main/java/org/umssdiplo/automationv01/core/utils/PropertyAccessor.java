615f98cd8e17b62004a0164a68fa8f355fd8a97d
rudolf <rudolf.flodur@gmail.com>
2019-04-21 22:03:32 -0400

    tarea restassure completada.

---
+++ src/main/java/org/umssdiplo/automationv01/core/utils/PropertyAccessor.java
@@ -0,0 +1,71 @@
+package org.umssdiplo.automationv01.core.utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyAccessor {
+    private static final String BROWSER = "browser";
+    private static final String BASE_URL = "baseurl";
+    private static final String USER_NAME = "username";
+    private static final String PASSWORD = "password";
+    private static final String BASE_URI = "postman.baseuri";
+
+    private static PropertyAccessor PropertyAccessor;
+    private Properties properties;
+
+    private PropertyAccessor() {
+        try (FileInputStream fileInputStream = new FileInputStream("gradle.properties")) {
+            properties = new Properties();
+            properties.load(fileInputStream);
+        } catch (FileNotFoundException fe) {
+            throw new RuntimeException(fe);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static PropertyAccessor getInstance() {
+        if (PropertyAccessor == null) {
+            PropertyAccessor = new PropertyAccessor();
+        }
+        return PropertyAccessor;
+    }
+
+    public int getImplicitTimeWait() {
+        return Integer.parseInt(getDataProperty("implicitTimeWait"));
+    }
+
+    private String getDataProperty(String nameProperty) {
+        String property = System.getProperty(nameProperty);
+        if (property == null) {
+            return properties.getProperty(nameProperty);
+        }
+        return property;
+    }
+
+    public int getExplicitTimeWait() {
+        return Integer.parseInt(getDataProperty("explicitTimeWait"));
+    }
+
+    public String getBrowser() {
+        return getDataProperty(BROWSER);
+    }
+
+    public String getBaseUrl() {
+        return getDataProperty(BASE_URL);
+    }
+
+    public String getUser() {
+        return getDataProperty(USER_NAME);
+    }
+
+    public String getPassword() {
+        return getDataProperty(PASSWORD);
+    }
+
+    public String getBaseUri() {
+        return getDataProperty(BASE_URI);
+    }
+}
---
+++ src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/PostManStepdefs.java
@@ -0,0 +1,38 @@
+package org.umssdiplo.automationv01.stepdefinitionproject;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.umssdiplo.automationv01.core.customrestassure.HandleRequest;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * @autor Marcelo Garay
+ */
+public class PostManStepdefs {
+    private Response response;
+
+    @Given("^GET \"([^\"]*)\" postman endpoint is configured$")
+    public void getPostmanEndpointIsConfigured(String headerEndpoint) throws Throwable {
+        response = HandleRequest.get(headerEndpoint);
+    }
+
+    @Then("^the status code should be (\\d+)$")
+    public void theStatusCodeShouldBe(int statusCode) throws Throwable {
+        assertEquals(response.getStatusCode(), statusCode);
+    }
+
+    @Then("^the responde 'data' value is \"([^\"]*)\"$")
+    public void theRespondeDataValueIs(String esperado) throws Throwable {
+        String actual = response.body().jsonPath().getString("data");
+
+        Assert.assertEquals(esperado, actual);
+    }
+
+    @Given("^POST \"([^\"]*)\" postman endpoint is configured$")
+    public void postPostmanEndpointIsConfigured(String arg0) throws Throwable {
+        response = HandleRequest.post(arg0, "diplomado testing");
+    }
+}
